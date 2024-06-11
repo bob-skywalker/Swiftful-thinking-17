e38769c942bd966b6dfe9ae874c7f636ccd8a96a
bo <q463250938@gmail.com>
2024-06-11 15:37:49 -0700

    codemagic CI/CD integration

---
+++ Swiftful-thinking-17/UnitTestingBootcampView.swift
@@ -0,0 +1,18 @@
+//
+//  UnitTestingBootcampView.swift
+//  Swiftful-thinking-17
+//
+//  Created by Bo Zhong on 6/11/24.
+//
+
+import SwiftUI
+
+struct UnitTestingBootcampView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnitTestingBootcampView()
+}
---
+++ Swiftful-thinking-17/UnitTestingBootcampViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UnitTestingBootcampViewModel.swift
+//  Swiftful-thinking-17
+//
+//  Created by Bo Zhong on 6/11/24.
+//
+
+import Foundation
