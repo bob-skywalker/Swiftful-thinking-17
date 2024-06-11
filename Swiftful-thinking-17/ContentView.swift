634765bafecca0d2a9b92489d553a0713f782b63
bo <q463250938@gmail.com>
2024-06-11 14:20:46 -0700

    Initial Commit

---
+++ Swiftful-thinking-17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftful-thinking-17
+//
+//  Created by Bo Zhong on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftful-thinking-17/Swiftful_thinking_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftful_thinking_17App.swift
+//  Swiftful-thinking-17
+//
+//  Created by Bo Zhong on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiftful_thinking_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
